9ab88192be73b513023f49a99fd7f27b6084b923
Kshitij Ingole <kshitijingole@kpmg.com>
2024-05-21 12:46:46 +0530

    React Router Links in Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // https://github.com/LinkedInLearning/react-creating-and-hosting-a-full-stack-site-3209140/tree/02_04b/my-blog
 import './App.css';
+import NavBar from './NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticleListPage from './pages/ArticleListPage';
@@ -10,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/about-us' element={<AboutPage />}></Route>
